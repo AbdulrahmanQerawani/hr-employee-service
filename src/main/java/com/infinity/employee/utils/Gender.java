292e06ed7d5fe0bcf300399ff061a93f643bcd8a
AbdulrahmanQerawani <Abdqarawani@gmail.com>
2023-02-07 15:39:27 +0200

    solve Gender class to accept json format request

--- src/main/java/com/infinity/employee/utils/Gender.java
+++ src/main/java/com/infinity/employee/utils/Gender.java
@@ -1,5 +1,20 @@
 package com.infinity.employee.utils;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Gender {
-    MALE, FEMALE
+    MALE("MALE"),
+    FEMALE("FEMALE"),
+    OTHER("OTHER");
+
+    private String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
